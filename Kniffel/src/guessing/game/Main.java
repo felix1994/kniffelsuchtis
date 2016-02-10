c1e078372e43ffc51a3327a6f7725ccd6b6e6233
Felix Fritzsche <felix.fritzsche@mbtech-group.com>
2016-02-10 14:05:40 +0100

    ui

--- Kniffel/src/guessing/game/Main.java
+++ Kniffel/src/guessing/game/Main.java
@@ -22,6 +22,7 @@ public class Main {
 	private Label lblAnzahlVersuche;
 	private Label versuche;
 	private  Label hint;
+	private Label lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -59,34 +60,47 @@ public class Main {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
-		shell.setLayout(new GridLayout(2, false));
+		shell.setLayout(new GridLayout(3, false));
+		new Label(shell, SWT.NONE);
 		
 		lblAnzahlVersuche = new Label(shell, SWT.NONE);
-		lblAnzahlVersuche.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblAnzahlVersuche.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
 		lblAnzahlVersuche.setText("Anzahl Versuche:");
 		
 		versuche = new Label(shell, SWT.NONE);
-		versuche.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		versuche.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1));
 		versuche.setText("0");
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		
+		lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1));
 		
 		text = new Text(shell, SWT.BORDER);
 		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
 		Button guess = new Button(shell, SWT.NONE);
+		guess.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
 		guess.setText("Guess!");
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		
 		hint = new Label(shell, SWT.NONE);
 		GridData gd_hint = new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1);
-		gd_hint.heightHint = 26;
+		gd_hint.heightHint = 28;
 		gd_hint.widthHint = 167;
 		hint.setLayoutData(gd_hint);
 		hint.setText("");
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
